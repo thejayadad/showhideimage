f7c673a0931f30429d2767cfb6156ad66d6817b6
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2022-09-16 08:00:07 -0400

    update

---
+++ main.js
@@ -0,0 +1,15 @@
+
+let a;
+function showPic(){
+    let hide = document.getElementById("hide");
+    if(a==1){
+        document.getElementById("pic").style.display="inline";
+        hide.style.display = "none";
+        return a = 0;
+    } else {
+        document.getElementById("pic").style.display = "none";
+        hide.innerHTML = "Its Hidden";
+        hide.style.textAlign = "center";
+        return a = 1;
+    }
+}
